c67e44498307d73a8929dac300352130a08cc8a7
Александр <rihor93@gmail.com>
2023-01-21 14:22:38 +0500

    добавил основные функции, осталось добавить тесты, storybook и обертку для обработки ошибок в компоненте

---
+++ src/gameOfLifeClasses/server.ts
@@ -0,0 +1,46 @@
+import { Cell, CellStatus } from "./CellComponentClasses";
+
+const width = 50;
+const heigth = 50;
+const cells = width * heigth;
+
+export {
+    width,
+    heigth,
+    cells
+}
+
+export async function getServerData(error: boolean) {
+
+    let board: Cell[] = [];
+    /**
+     * генератор случайных чисел
+     * @param max максимальное значение, для генератора,возвращает знание от 0 до max
+     * @returns 
+     */
+    function getRandomInt(max: number): number {
+        return Math.floor(Math.random() * max);
+    }
+
+    /**
+     * функция инициализации начального состоянии игры
+     */
+    function randomBoard(): void {
+        board = [];
+        for (var i = 0; i < cells; i++) {
+            let randomVal = getRandomInt(2);
+            if (randomVal === 0) {
+                board[i] = { id: i, status: CellStatus.Dead };
+            } else {
+                board[i] = { id: i, status: CellStatus.Alive };
+            }
+            //board[i] = { id: i, status: CellStatus.Dead };
+        }
+    }
+
+    randomBoard();
+    if (error) {
+        throw 'Ошибка сервера'
+    }
+    return board;
+}
